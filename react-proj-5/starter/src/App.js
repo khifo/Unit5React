20ef3614fb0ce32ba108cae9810bac7fafe1ece0
khifo <khifo09@yahoo.com>
2023-07-18 19:10:34 -0600

    part 4

--- react-proj-5/starter/src/App.js
+++ react-proj-5/starter/src/App.js
@@ -1,9 +1,13 @@
+import { useSelector } from "react-redux";
 import "./App.css";
 import Header from "./Components/Header";
 import MainDisplay from "./Components/MainDisplay";
 import OptionDisplay from "./Components/OptionDisplay";
+import { selectDisplay } from "./redux/slices/displayCountrySlice";
 
 function App() {
+    let currentDisplay = useSelector(selectDisplay);
+    console.log("DISPLAY", currentDisplay);
     return (
         <div className="App font-link">
             <Header />
--- react-proj-5/starter/src/redux/store.js
+++ react-proj-5/starter/src/redux/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import potentialCountriesReducer from "./slices/potentialCountriesSlice";
+import displayedCountryReducer from "../redux/slices/displayCountrySlice"
 
 export default configureStore({
-    reducer: { potentialCountries: potentialCountriesReducer },    
+    reducer: { potentialCountries: potentialCountriesReducer,
+               displayedCountry: displayedCountryReducer, 
+    },    
 });
