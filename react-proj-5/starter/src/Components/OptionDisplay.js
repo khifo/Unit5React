95d5e955a3b89c9d78ad3ee19de627cea92b37d0
khifo <khifo09@yahoo.com>
2023-07-18 18:54:11 -0600

    part 3

--- react-proj-5/starter/src/Components/OptionDisplay.js
+++ react-proj-5/starter/src/Components/OptionDisplay.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectPotentials } from "../redux/slices/potentialCountriesSlice";
 
 const OptionDisplay = () => {
+    let currentPotentials = useSelector(selectPotentials);
+    console.log(currentPotentials);
     return <div className="stack">OptionDisplay</div>;
 };
 
