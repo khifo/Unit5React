73fed3c1b258ede4ffb403de42e2298f30e10f4d
khifo <khifo09@yahoo.com>
2023-07-18 19:25:13 -0600

    part 5

--- react-proj-5/starter/src/Components/Header.js
+++ react-proj-5/starter/src/Components/Header.js
@@ -1,14 +1,20 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { BsFillFlagFill } from "react-icons/bs";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
     deletePotentialCountries,
     setPotentialCountries,
 } from "../redux/slices/potentialCountriesSlice";
+import {
+    selectDisplay,
+    deleteDisplayCountry,
+} from "../redux/slices/displayCountrySlice";
+
 const Header = () => {
     const [input, setInput] = useState();
     let dispatch = useDispatch();
+    let currentDisplay = useSelector(selectDisplay);
     return (
         <div className="header">
             <div className="home">
@@ -16,7 +22,9 @@ const Header = () => {
                     style={{ marginRight: "10px" }}
                     fontSize="1.6em"
                 />
-                <h3 className="home-country"></h3>
+                <h3 className="home-country">
+                    {currentDisplay && currentDisplay.name.common}
+                </h3>
             </div>
             <div className="country-input">
                 <input
@@ -30,6 +38,7 @@ const Header = () => {
                             .get(`https://restcountries.com/v3.1/name/${input}`)
                             .then((res) => {
                                 console.log(res.data);
+                                dispatch(deleteDisplayCountry());
                                 dispatch(deletePotentialCountries());
                                 dispatch(setPotentialCountries());
                             })
--- react-proj-5/starter/src/Components/OptionDisplay.js
+++ react-proj-5/starter/src/Components/OptionDisplay.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { useDispatch,useSelector } from "react-redux";
 import { useSelector } from "react-redux";
 import { selectPotentials } from "../redux/slices/potentialCountriesSlice";
 
 const OptionDisplay = () => {
     let currentPotentials = useSelector(selectPotentials);
     console.log(currentPotentials);
-    return <div className="stack">OptionDisplay</div>;
+    const dispatch = useDispatch();
+    return <div className="stack">
+        {currentPotentials.map((e,i) => {
+            return (
+                <h2 
+                    key={e.name.official}
+                    className="country-option"
+                    onClick={() => {
+                        dispatch(setDisplayCountry(currentPotentials[i]));
+                    }}
+                    >
+                        {e.name.common}
+                    </h2>
+            );
+        })}
+        </div>;
 };
 
 export default OptionDisplay;
